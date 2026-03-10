b76c25c3c037ad27dd9d49a38a2a464764f6fdc2
Ahmadkhaan08 <mrahmadismail96@gmail.com>
2026-03-10 23:01:29 +0500

    Enhance ProductDescription component with TabsTrigger for improved UI navigation

--- client/src/components/product/ProductDescription.tsx
+++ client/src/components/product/ProductDescription.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Product } from "@/types/type";
 import React, { useState } from "react";
-import { Tabs, TabsList } from "../ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
 
 interface Props {
   product: Product;
@@ -9,9 +9,16 @@ interface Props {
 const ProductDescription = ({ product }: Props) => {
   const [active, setActive] = useState("description");
   return (
-    <div>
-      <Tabs>
-        <TabsList></TabsList>
+    <div className="w-full">
+      <Tabs defaultValue="description">
+        <TabsList>
+          <TabsTrigger
+            value="description"
+            className="py-2 text-babyshopBlack hover:text-babyshopSky rounded-lg transition-all data-[state=active]:bg-babyshopSky data-[state=active]:text-babyshopWhite "
+          >
+            Description
+          </TabsTrigger>
+        </TabsList>
       </Tabs>
     </div>
   );
