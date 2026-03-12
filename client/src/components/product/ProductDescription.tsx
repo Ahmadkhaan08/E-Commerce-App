159ce5e301468d5ec1d807741eb9bdcb406a8326
Ahmadkhaan08 <mrahmadismail96@gmail.com>
2026-03-12 22:33:07 +0500

    Add Reviews tab to ProductDescription component for improved user feedback

--- client/src/components/product/ProductDescription.tsx
+++ client/src/components/product/ProductDescription.tsx
@@ -24,6 +24,12 @@ const ProductDescription = ({ product }: Props) => {
           >
             About the Brand
           </TabsTrigger>
+          <TabsTrigger
+            value="brand"
+            className="py-2 text-babyshopBlack hover:text-babyshopSky rounded-lg transition-all data-[state=active]:bg-babyshopSky data-[state=active]:text-babyshopWhite "
+          >
+            Reviews (0)
+          </TabsTrigger>
         </TabsList>
       </Tabs>
     </div>
